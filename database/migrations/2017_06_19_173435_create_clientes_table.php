b78f9bc0674b49aaa3b2308f05a9ce1a9c83738b
antonio1394 <antoniorivm1394@gmail.com>
2017-07-20 23:09:34 -0600

    [update]

--- database/migrations/2017_06_19_173435_create_clientes_table.php
+++ database/migrations/2017_06_19_173435_create_clientes_table.php
@@ -18,6 +18,9 @@ class CreateClientesTable extends Migration
             $table->string('apellido');
             $table->string('direccion');
             $table->string('NIT');///restirngir numero
+
+
+            
             $table->timestamps();
 
         });
